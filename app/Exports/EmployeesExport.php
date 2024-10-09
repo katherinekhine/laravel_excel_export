925b40076d03ec317e16fe1f755f6edf3e0964b7
katherinekhine <khaingkatherine@gmail.com>
2024-10-09 19:53:54 +0630

    update EmployeeExport.php

--- app/Exports/EmployeesExport.php
+++ app/Exports/EmployeesExport.php
@@ -3,15 +3,27 @@
 namespace App\Exports;
 
 use App\Models\Employee;
+use Illuminate\Support\Arr;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class EmployeesExport implements FromCollection
+class EmployeesExport implements FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        return Employee::all();
+        return Employee::select('id', 'name', 'email', 'phone')->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'Name',
+            'Email',
+            'Phone',
+        ];
     }
 }
